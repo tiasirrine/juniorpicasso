5b38e48b35721335c62bf89f3ab5cc7af1fb5335
AZCoder018 <jason.meadows.esq@hotmail.com>
2018-05-19 12:01:34 -0700

    adding changes to branch

---
+++ app/public/js/register.js
@@ -0,0 +1,33 @@
+
+
+// The code in add.js handles what happens when the user clicks the "Submit" button to register as a new member.
+
+// When user clicks add-btn
+$("#add-btn").on("click", function(event) {
+  event.preventDefault();
+
+  // Make a newMember object
+  var newMember = {
+    first_name: $("#first_name").val().trim(),
+    last_name: $("#last_name").val().trim(),
+    address: $("#address").val().trim(),
+    email: $("#email").val().trim(),
+    password: $("#password").val().trim()
+  };
+
+  // Send an AJAX POST-request with jQuery
+  $.post("/api/new", newMember)
+    // On success, run the following code
+    .then(function(data) {
+      // Log the data we found
+      console.log(data);
+    });
+
+  // Empty each input box by replacing the value with an empty string
+  $("#first_name").val("");
+  $("#last_name").val("");
+  $("#address").val("");
+  $("#email").val("");
+  $("#password").val("");
+
+});
--- server.js
+++ server.js
@@ -1,119 +1,34 @@
+// *********************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
-const bcrypt = require("bcrypt");
 
-var connection = require("./config/connection.js");
-
+// Sets up the Express App
+// =============================================================
+var app = express();
 var PORT = process.env.PORT || 8080;
 
-var app = express();
-
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+// Sets up the Express app to handle data parsing
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-
 // parse application/json
 app.use(bodyParser.json());
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
+// Static directory
+app.use(express.static("app/public"));
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+// Routes
+// =============================================================
+require("./app/routes/api-routes.js")(app);
+require("./app/routes/html-routes.js")(app);
 
-// Import routes and give the server access to them.
-//var routes = require("#");
-
-//app.use(routes);
-app.get("/", function(req, res) {
-  res.render("index");
-});
-
-app.get("/login", function(req, res) {
-  res.render("login");
-});
-
-app.get("/signup", function(req, res) {
-  res.render("signup");
-});
-
-app.get("/search", function(req, res) {
-  res.render("index");
-});
-
-app.get("/about", function(req, res) {
-  res.render("index");
-});
-
-app.get("/vote", function(req, res) {
-  res.render("index");
-});
-
-app.post("/signup", function(req, res) {
-  console.log('req.body', req.body); // { email: 'blah', password: 'ho noora!' }
-
-  // object destructuring
-  var { firstName, lastName, email, password } = req.body;
-  //var email = req.body.email;
-  //var password = req.body.password;
-  var hash = bcrypt.hashSync(password, 10);
-
-  var queryString = `INSERT INTO users (first_name, last_name, email, password) VALUES ("${firstName}", "${lastName}", "${email}", "${hash}");`;
-
-  console.log(queryString);
-
-  connection.query(queryString, function(err, result) {
-    if (err) {
-      throw err;
-    }
-  });
-  return "success";
-});
-
-app.post("/login", function(req, res) {
-  console.log('req.body', req.body); // { email: 'blah', password: 'ho noora!' }
-
-  // object destructuring
-  var { email, password } = req.body;
-  //var email = req.body.email;
-  //var password = req.body.password;
-
-  var queryString = `SELECT * FROM users WHERE email = "${email}"`;
-
-
-
-  console.log(queryString);
-
-  connection.query(queryString, function(err, result) {
-    if (err) {
-      throw err;
-    }
-    console.log('result', result);
-    //return result;
-    bcrypt.compare(password, result[0].password, function(err, match){
-     console.log(password, result[0].password);
-
-      if (match) {
-        // Passwords match
-        console.log("It's a match!!");
-        res.send(result);
-        res.render("/index.html");
-    } else {
-        // Passwords don't match
-        console.log("Wrong password");
-    }
-    });
-
-
-   
-  });
-});
-
-
-// Start our server so that it can begin listening to client requests.
+// Starts the server to begin listening
+// =============================================================
 app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+  console.log("App listening on PORT " + PORT);
 });
